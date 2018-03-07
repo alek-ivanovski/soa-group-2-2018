144c308befbdccd7ea990b1176aa47cbf69d8ead
Alek Ivanovski <alek.ivanovski96@gmail.com>
2018-03-07 16:34:00 +0100

    Initial commit

---
+++ src/main/java/soa/group2/soagroup2/SoaGroup2Application.java
@@ -0,0 +1,12 @@
+package soa.group2.soagroup2;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SoaGroup2Application {
+
+	public static void main(String[] args) {
+		SpringApplication.run(SoaGroup2Application.class, args);
+	}
+}
---
+++ src/test/java/soa/group2/soagroup2/SoaGroup2ApplicationTests.java
@@ -0,0 +1,16 @@
+package soa.group2.soagroup2;
+
+import org.junit.Test;
+import org.junit.runner.RunWith;
+import org.springframework.boot.test.context.SpringBootTest;
+import org.springframework.test.context.junit4.SpringRunner;
+
+@RunWith(SpringRunner.class)
+@SpringBootTest
+public class SoaGroup2ApplicationTests {
+
+	@Test
+	public void contextLoads() {
+	}
+
+}
