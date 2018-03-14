34abc97dd3f16df85647be3aaf624da4bc44c3f5
Filip Simonovski <pipe.fanny@gmail.com>
2018-03-14 16:26:18 +0100

    created presistance layer

--- src/main/java/soa/group2/soagroup2/model/Event.java
+++ src/main/java/soa/group2/soagroup2/model/Event.java
@@ -3,22 +3,34 @@ package soa.group2.soagroup2.model;
 import javax.persistence.Entity;
 import javax.persistence.GeneratedValue;
 import javax.persistence.Id;
+import javax.persistence.ManyToMany;
 import java.util.Date;
+import java.util.List;
 
 @Entity
 public class Event {
 
     @Id
     @GeneratedValue
-    public Long id;
+    public long id;
 
-    public String title;
+    public String name;
 
     public String description;
 
     public String location;
 
     public Date date;
+
+    public Integer priority;
+
+    @ManyToMany
+    List<User> participants;
+
+    @ManyToMany
+    List<User> organizers;
+
+
 
 
 }
--- src/main/java/soa/group2/soagroup2/service/CalendarService.java
+++ src/main/java/soa/group2/soagroup2/service/CalendarService.java
@@ -1,4 +1,17 @@
 package soa.group2.soagroup2.service;
 
+import soa.group2.soagroup2.model.Event;
+import soa.group2.soagroup2.model.User;
+
+import java.util.List;
+
 public interface CalendarService {
+
+    public void createEvent(Event event);
+    public void updateEvent(Event event);
+    public void deleteEvent(long eventID);
+
+    public List<Event> showEvents(User user);
+
+
 }
