ff0bea58e58312123280acd9a5fdfaf17103c6c2
Filip Simonovski <pipe.fanny@gmail.com>
2018-04-11 17:27:48 +0200

    joblisting controller done

--- src/main/java/com/soa/recruitment/web/controller/JobListingController.java
+++ src/main/java/com/soa/recruitment/web/controller/JobListingController.java
@@ -1,10 +1,13 @@
 package com.soa.recruitment.web.controller;
 
+import com.soa.recruitment.model.JobApplication;
 import com.soa.recruitment.model.JobListing;
 import com.soa.recruitment.service.JobListingService;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.http.MediaType;
 import org.springframework.web.bind.annotation.*;
+
+import java.util.Optional;
 
 @RestController
 @RequestMapping(value = "/recruitment/jobs/listing", produces = MediaType.APPLICATION_JSON_VALUE, consumes = MediaType.APPLICATION_JSON_VALUE)
@@ -27,9 +30,15 @@ public class JobListingController {
         return jobListingService.save(category);
     }
 
-    @RequestMapping(value = "/{id}", method = RequestMethod.DELETE)
+    @RequestMapping(value = "delete/{id}", method = RequestMethod.DELETE)
     public void delete(@PathVariable Long id){
         jobListingService.delete(id);
+    }
+
+    @RequestMapping(value = "/{id}", method = RequestMethod.GET)
+    public Optional<JobListing> getJob(@PathVariable Long id) {
+        return jobListingService.findOne(id);
+
     }
 
 
